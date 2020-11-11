5b06e113f8c869528cd706ec00b614a62234ea14
FanQinFred <1836459118@qq.com>
2020-11-11 16:51:20 +0800

    待添加合并IP包信息功能

--- code/network_layer/network_layer_send.cpp
+++ code/network_layer/network_layer_send.cpp
@@ -1,9 +1,3 @@
-// 数据链路层
-void datalink_layer_send(unsigned char* buf,int len){
-
-}
-//
-
 /*
   @函数采用帕斯卡命名法
 */
@@ -11,6 +5,46 @@ void datalink_layer_send(unsigned char* buf,int len){
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h>
+
+// 数据链路层S
+// Mac Address
+typedef unsigned char mac_addr[6];
+mac_addr DesMacAddr = { 0x3D,0xE1,0x2D,0x6F,0xE9,0x34 };
+mac_addr SrcMacAddr = { 0x34,0xE1,0x2D,0x6F,0xE9,0x3D };
+
+unsigned int crc32(unsigned char* data, int len) {
+    unsigned int crc = 0xFFFFFFFF;
+    for (int i = 0;i < len;i++) {
+        crc = crc ^ data[i];
+        for (int j = 0;j < 8;j++) {
+            crc = (crc >> 1) ^ (0xEDB88320 & (-(crc & 1)));
+        }
+    }
+    return ~crc;
+}
+
+// Merge data to form frames
+unsigned short make_frame(mac_addr* dst, mac_addr* src, unsigned short protocol, unsigned char* payload, int payloadlen, unsigned char* result) {
+    memcpy(&result[0], dst, 6);
+    memcpy(&result[6], src, 6);
+    memcpy(&result[12], &protocol, sizeof(protocol));
+    memcpy(&result[14], payload, payloadlen);
+    unsigned int crc32_result = crc32(result, payloadlen + 14);
+    memcpy(&result[14 + payloadlen], &crc32_result, sizeof(crc32_result));
+    return 18 + payloadlen;
+}
+// Send a frame
+void send_frame(unsigned char* frame_data, unsigned short len, FILE* file) {
+    fwrite(&len, sizeof(len), 1, file);
+    //frame_data[len-1]=~frame_data[len-1];
+    fwrite(frame_data, sizeof(char), len, file);
+}
+
+void datalink_layer_send(unsigned char* buf,int len){
+
+}
+// 数据链路层E
+
 
 #define DATA_MAXSIZE 1440  //字节
 #define DATA_MINSIZE 26    //字节
@@ -140,7 +174,7 @@ void HeaderSetCheckSum(IP_Packet &ip_packet) {
 // Add data to IP packet
 unsigned int AddDataToPacket(IP_Packet &ip_packet,unsigned char *data, long int len,unsigned char* buf){
   memcpy(ip_packet.IPv4_Data,data,len);
-
+  
   return 60+len;
 }
 
