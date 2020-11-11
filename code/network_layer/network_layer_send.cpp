95dd951385d01dc3606e49c92a1c79056efc5166
FanQinFred <1836459118@qq.com>
2020-11-11 17:55:08 +0800

    send基本结束

--- code/network_layer/network_layer_send.cpp
+++ code/network_layer/network_layer_send.cpp
@@ -51,7 +51,7 @@ void datalink_layer_send(unsigned char* buf,int len){
 
 struct IP_Packet{
   // 4 bit
-  unsigned int IPv4_version:4;
+  unsigned int IPv4_Version:4;
   // 4 bit
   unsigned int IPv4_IHL:4;
   // 8 bit
@@ -144,11 +144,25 @@ unsigned short extendr_16bit(unsigned int bit_content,int bit_offset){
   return res;
 }
 
+unsigned short extendl_8bit(unsigned int bit_content,int bit_offset){
+  bit_content=bit_content<<bit_offset;
+  unsigned char res=0b0; //16 bit
+  res+=bit_content;
+  return res;
+}
+
+unsigned short extendr_8bit(unsigned int bit_content,int bit_offset){
+  bit_content=bit_content>>bit_offset;
+  unsigned char res=0b0; //16 bit
+  res+=bit_content;
+  return res;
+}
+
 // CheckSum
 void HeaderSetCheckSum(IP_Packet &ip_packet) {
   ip_packet.IPv4_HeaderCheckSum=
 
-  extendl_16bit(ip_packet.IPv4_version,12)+
+  extendl_16bit(ip_packet.IPv4_Version,12)+
   extendl_16bit(ip_packet.IPv4_IHL,8)+
   extendl_16bit(ip_packet.IPv4_TOS,0)+
 
@@ -171,17 +185,50 @@ void HeaderSetCheckSum(IP_Packet &ip_packet) {
   extendr_16bit(ip_packet.IPv4_DesAddr,16);
 }
 
-// Add data to IP packet
-unsigned int AddDataToPacket(IP_Packet &ip_packet,unsigned char *data, long int len,unsigned char* buf){
-  memcpy(ip_packet.IPv4_Data,data,len);
-  
-  return 60+len;
-}
-
 // Make Ip packet
-void MakeIpPacket(){
-
+unsigned long MakeIpPacket(const IP_Packet ip_packet,unsigned char* buf,unsigned char* IPv4_Option,long IPv4_Option_Len,unsigned char* IPv4_Data,long IPv4_Data_Len){
+  //第一个byte
+  unsigned char VersionAndIhl=extendl_8bit(ip_packet.IPv4_Version,4)+extendl_8bit(ip_packet.IPv4_IHL,0); //8 bit
+  memcpy(buf,&VersionAndIhl,sizeof(VersionAndIhl));
+  //第二个byte
+  unsigned char IPv4_TOS=extendl_8bit(ip_packet.IPv4_TOS,0); //8 bit
+  memcpy(&buf[1],&IPv4_TOS,sizeof(IPv4_TOS));  //1已经被占用
+  //第三四个byte
+  unsigned short IPv4_TotalLength=ip_packet.IPv4_TotalLength;
+  memcpy(&buf[2],&IPv4_TotalLength,sizeof(IPv4_TotalLength)); //3已经被占用
+  //第五六个byte
+  unsigned short IPv4_Identification=ip_packet.IPv4_Identification;
+  memcpy(&buf[4],&IPv4_Identification,sizeof(IPv4_Identification)); //5已经被占用
+  //第7、8个byte
+  unsigned short NoFunc_DF_FragmentOffset=extendl_16bit(ip_packet.IPv4_NoFunc,15)+extendl_16bit(ip_packet.IPv4_DF,14)+extendl_16bit(ip_packet.IPv4_MF,13)+extendl_16bit(ip_packet.IPv4_FragmentOffset,0);
+  memcpy(&buf[6],&NoFunc_DF_FragmentOffset,sizeof(NoFunc_DF_FragmentOffset)); //7已经被占用
+  //第9、10个byte
+  unsigned short TimeTolive_Protocol=extendl_16bit(ip_packet.IPv4_TimeTolive,8)+extendl_16bit(ip_packet.IPv4_Protocol,0);
+  memcpy(&buf[8],&TimeTolive_Protocol,sizeof(TimeTolive_Protocol)); //9已经被占用
+  //11 12
+  unsigned short HeaderCheckSum=ip_packet.IPv4_HeaderCheckSum;
+  memcpy(&buf[10],&HeaderCheckSum,sizeof(HeaderCheckSum)); //11已经被占用
+  //13 14 15 16
+  unsigned int SourceAddr=ip_packet.IPv4_SourceAddr;
+  memcpy(&buf[12],&SourceAddr,sizeof(SourceAddr)); //15已经被占用
+  //17 18 19 20
+  unsigned int DesAddr=ip_packet.IPv4_DesAddr;
+  memcpy(&buf[16],&DesAddr,sizeof(DesAddr)); //19已经被占用
+  // option
+  memcpy(&buf[20],IPv4_Option,IPv4_Option_Len);
+  // data
+  memcpy(&buf[20+IPv4_Option_Len],IPv4_Data,IPv4_Data_Len);
+  //返回字节数
+  return 20+IPv4_Option_Len+IPv4_Data_Len;
 }
+
+
+// Add data to IP packet
+unsigned long AddDataToPacket(IP_Packet &ip_packet,unsigned char *IPv4_Data, long int IPv4_Data_Len,unsigned char* IPv4_Option,long IPv4_Option_Len,unsigned char* buf){
+  memcpy(ip_packet.IPv4_Data,IPv4_Data,IPv4_Data_Len);
+  return MakeIpPacket(ip_packet,buf,IPv4_Option,IPv4_Option_Len,IPv4_Data,IPv4_Data_Len);
+}
+
 
 void start_send(IP_Packet ip_packet_info, char* fileinput, char* fileoutput) {
     long int i;
@@ -193,30 +240,30 @@ void start_send(IP_Packet ip_packet_info, char* fileinput, char* fileoutput) {
     rewind(fileIn);
     // Split the data of the file, then each data is processed after segmentation
     for (long int j = 0; j <= PayLoadCount; j++) {
-        unsigned char data[DATA_MAXSIZE];
-        unsigned char buf[1536];
+        unsigned char IPv4_Data[DATA_MAXSIZE];
+        unsigned char buf[DATA_MAXSIZE+60];
         int FrameLength;
         // Not the last frame
         if (j != PayLoadCount) {
-            fread(data, sizeof(char), DATA_MAXSIZE, fileIn);
-            unsigned int IpPacketLen=AddDataToPacket(ip_packet_info,data,DATA_MAXSIZE,buf);
+            fread(IPv4_Data, sizeof(char), DATA_MAXSIZE, fileIn);
+            unsigned long IpPacketLen=MakeIpPacket(ip_packet_info,buf,ip_packet_info.IPv4_Option,40,IPv4_Data,DATA_MAXSIZE);//AddDataToPacket(ip_packet_info,data,DATA_MAXSIZE,buf);
             datalink_layer_send(buf, IpPacketLen);
         }
         else {
             LastFramePos = ftellSafe(fileIn);
             RestByteCount = AllByteCount - PayLoadCount * 1500;
             LackByteCount = DATA_MINSIZE - RestByteCount;
-            fread(data, sizeof(char), AllByteCount - LastFramePos, fileIn);
-            // The frame size is less than 46 bytes
+            fread(IPv4_Data, sizeof(char), AllByteCount - LastFramePos, fileIn);
+            // The frame size is less than 26 bytes
             if (LackByteCount > 0) {
               for (i = 0;i < LackByteCount; i++) {
-                data[RestByteCount++] = 0x00;
+                IPv4_Data[RestByteCount++] = 0x00;
               }
-              unsigned int IpPacketLen=AddDataToPacket(ip_packet_info,data,DATA_MINSIZE,buf);
+              unsigned long IpPacketLen=MakeIpPacket(ip_packet_info,buf,ip_packet_info.IPv4_Option,40,IPv4_Data,DATA_MINSIZE);//AddDataToPacket(ip_packet_info,data,DATA_MAXSIZE,buf);
               datalink_layer_send(buf, IpPacketLen);
             }
             else {
-              unsigned int IpPacketLen=AddDataToPacket(ip_packet_info,data,RestByteCount,buf);
+              unsigned long IpPacketLen=MakeIpPacket(ip_packet_info,buf,ip_packet_info.IPv4_Option,40,IPv4_Data,RestByteCount);//AddDataToPacket(ip_packet_info,data,DATA_MAXSIZE,buf);
               datalink_layer_send(buf, IpPacketLen);
             }
         }
@@ -229,7 +276,7 @@ void start_send(IP_Packet ip_packet_info, char* fileinput, char* fileoutput) {
 
 // Test
 int main(){
-	struct IP_Packet ip_packet={0b0100,0b0101,0b00000000,  //IPv4_version,IPv4_IHL,IPv4_TOS
+	struct IP_Packet ip_packet={0b0100,0b0101,0b00000000,  //IPv4_Version,IPv4_IHL,IPv4_TOS
                               0b0000000000000000,  //IPv4_TotalLength
                               0b0000000000000000,  //IPv4_Identification
                               0b0,0b0,0b0,0b0000000000000,  //IPv4_NoFunc,IPv4_DF,IPv4_MF,IPv4_FragmentOffset
